9197258eb2c9dbb0a35e718b554f3f6277f06f0b
Sergey Klyaus <myautneko@gmail.com>
2013-12-30 16:14:51 +0400

    http: notblocking netsockets

--- http/include/http.h
+++ http/include/http.h
@@ -8,30 +8,7 @@
 #ifndef MOD_LOAD_HTTP_
 #define MOD_LOAD_HTTP_
 
-/* FIXME: Should move socket API to tscommon */
-#include <plat/client.h>
-
-plat_host_entry* plat_clnt_resolve(const char* host);
-int plat_clnt_setaddr(plat_clnt_addr* clnt_sa, plat_host_entry* he, int clnt_port);
-
-int plat_clnt_connect(plat_clnt_socket* clnt_socket, plat_clnt_addr* clnt_sa);
-int plat_clnt_disconnect(plat_clnt_socket* clnt_socket);
-
-int plat_clnt_poll(plat_clnt_socket* clnt_socket, ts_time_t timeout);
-
-int plat_clnt_send(plat_clnt_socket* clnt_socket, void* data, size_t len);
-int plat_clnt_recv(plat_clnt_socket* clnt_socket, void* data, size_t len);
-
-#define CLNT_OK				0
-#define CLNT_ERR_RESOLVE	-1
-#define CLNT_ERR_SOCKET		-2
-#define CLNT_ERR_CONNECT	-3
-
-#define CLNT_POLL_OK			0
-#define CLNT_POLL_NEW_DATA		1
-#define CLNT_POLL_DISCONNECT 	2
-#define CLNT_POLL_FAILURE		3
-
+#include <netsock.h>
 #include <wlparam.h>
 
 #define MAXHOSTNAMELEN	256
@@ -45,8 +22,11 @@ struct http_workload {
 	wlp_integer_t 	port;
 	wlp_string_t 	url[MAXURLLEN];
 
-	plat_clnt_addr		clnt_addr;
+	nsk_addr		clnt_addr;
+	nsk_host_entry  he;
+
 	int url_len;
+	int server_len;
 
 	long logged_step;
 	int logged_code;
--- http/src/http.c
+++ http/src/http.c
@@ -25,6 +25,20 @@ DECLARE_MODAPI_VERSION(MOD_API_VERSION);
 DECLARE_MOD_NAME("http");
 DECLARE_MOD_TYPE(MOD_TSLOAD);
 
+#define MSGLEN		128
+
+typedef struct {
+	int http_minor_version;
+	int http_code;
+
+	int header_length;
+	int content_length;
+
+	char message[MSGLEN];
+} http_header_t;
+
+static int parse_http_header(char* response_buf, http_header_t* header);
+
 MODEXPORT wlp_descr_t http_params[] = {
 	{ WLP_RAW_STRING, WLPF_NO_FLAGS,
 		WLP_STRING_LENGTH(MAXHOSTNAMELEN),
@@ -55,12 +69,10 @@ thread_mutex_t resolver_mutex;
 MODEXPORT int http_wl_config(workload_t* wl) {
 	struct http_workload* hww =
 				(struct http_workload*) wl->wl_params;
-	plat_host_entry* he = NULL;
 
 	mutex_lock(&resolver_mutex);
-	he = plat_clnt_resolve(hww->server);
-
-	if(plat_clnt_setaddr(&hww->clnt_addr, he, hww->port) != CLNT_OK) {
+	if(nsk_resolve(hww->server, &hww->he) != NSK_OK ||
+	   nsk_setaddr(&hww->clnt_addr, &hww->he, hww->port) != NSK_OK) {
 		mutex_unlock(&resolver_mutex);
 		return 1;
 	}
@@ -68,6 +80,8 @@ MODEXPORT int http_wl_config(workload_t* wl) {
 	mutex_unlock(&resolver_mutex);
 
 	hww->url_len = strlen(hww->url);
+	hww->server_len = strlen(hww->server);
+
 	hww->logged_code = 200;
 	hww->logged_step = -1;
 
@@ -88,29 +102,33 @@ MODEXPORT int http_run_request(request_t* rq) {
 
 	int ret = 0;
 	int status = 0;
-	int recv_len = 0;
+	int recv_len = 0, recv_ret = -1;
 	int bufnum = 0;
-
-	int http_minor_ver;
-	int http_code;
 
 	boolean_t receiving = B_FALSE, connected = B_TRUE;
 
-	plat_clnt_socket socket;
+	nsk_socket socket;
+
+	http_header_t header;
 
 	/* Connect */
-	if(plat_clnt_connect(&socket, &hww->clnt_addr) != CLNT_OK) {
+	if(nsk_connect(&socket, &hww->clnt_addr, NSK_STREAM) != NSK_OK) {
 		return 1;
 	}
 
+	nsk_setnb(&socket, B_TRUE);
+
 	/* Send HTTP request */
-	request_len = hww->url_len + 48;
+	request_len = hww->url_len + hww->server_len + 55;
 	request = malloc(request_len);
 
-	snprintf(request, request_len, "GET %s HTTP/1.1\n"
-			"User-Agent: TSLoad HTTP Module\n\n", hww->url);
+	snprintf(request, request_len,
+			"GET %s HTTP/1.1\n"
+			"Host: %s\n"
+			"User-Agent: TSLoad HTTP Module\n\n",
+				hww->url, hww->server);
 
-	plat_clnt_send(&socket, request, request_len);
+	nsk_send(&socket, request, request_len);
 
 	free(request);
 
@@ -119,47 +137,51 @@ MODEXPORT int http_run_request(request_t* rq) {
 
 	while(connected) {
 		if(receiving) {
-			recv_len = plat_clnt_recv(&socket, response_buf, RESPONSE_BUF_SIZE);
-			receiving = B_FALSE;
+			recv_ret = nsk_recv(&socket, response_buf, RESPONSE_BUF_SIZE);
 
-			if(recv_len == 0) {
-				connected = B_FALSE;
+			if(recv_ret < 0)
 				break;
-			}
+
+			recv_len -= recv_ret;
+			receiving = B_FALSE;
 
 			/* Read status code if first buffer to get */
 			if(bufnum++ == 0) {
-				char* eol = NULL;
-				sscanf(response_buf, "HTTP/1.%d %d", &http_minor_ver, &http_code);
-
-				eol = strchr(response_buf, '\n');
-				if(eol != NULL) {
-					*eol = '\0';
+				if(parse_http_header(response_buf, &header) == 1) {
+					connected = B_FALSE;
+					break;
 				}
+
+				recv_len += header.header_length + header.content_length;
 
 				/* Log error, but not frequently than 1 code per step.
 				 * But if codes are random, that still overflows log */
-				if(http_code != 200 &&
+				if(header.http_code != 200 &&
 				   ((rq->rq_step > hww->logged_step) ||
-					 hww->logged_code != http_code)) {
+					 hww->logged_code != header.http_code)) {
 						logmsg(LOG_ERROR, "got response: %s", response_buf);
 
-						hww->logged_code = http_code;
+						hww->logged_code = header.http_code;
 						hww->logged_step = rq->rq_step;
 				}
 			}
+
+			/* Received all, disconnect */
+			if(recv_len <= 0) {
+				break;
+			}
 		}
 
-		status = plat_clnt_poll(&socket, 0);
+		status = nsk_poll(&socket, 10 * T_MS);
 
 		switch(status) {
-		case CLNT_POLL_FAILURE:
+		case NSK_POLL_FAILURE:
 			ret = 1;
 			/* FALLTHROUGH */
-		case CLNT_POLL_DISCONNECT:
+		case NSK_POLL_DISCONNECT:
 			connected = B_FALSE;
 			break;
-		case CLNT_POLL_NEW_DATA:
+		case NSK_POLL_NEW_DATA:
 			receiving = B_TRUE;
 			break;
 		}
@@ -168,7 +190,45 @@ MODEXPORT int http_run_request(request_t* rq) {
 	free(response_buf);
 
 	/* Release resources */
-	plat_clnt_disconnect(&socket);
+	nsk_disconnect(&socket);
+
+	return ret;
+}
+
+static int parse_http_header(char* response_buf, http_header_t* header) {
+	char* eol = NULL;
+	char* eoh = response_buf;
+	char* p = NULL;
+	size_t pos = 0;
+	size_t data_len;
+	int http_minor_ver;
+
+	int ret = 1;
+
+	sscanf(response_buf, "HTTP/1.%d %d %128s\n",
+			&header->http_minor_version,
+			&header->http_code,
+			header->message);
+
+	while(pos < (RESPONSE_BUF_SIZE - 4)) {
+		if(eol == NULL && eoh[0] == '\n')
+			eol = eoh;
+
+		if(eoh[0] == 'C')
+			sscanf(eoh, "Content-Length: %d", &header->content_length);
+
+		if(strncmp(eoh, "\r\n\r\n", 4) == 0) {
+			header->header_length = eoh - response_buf + 4;
+
+			ret = 0;
+			break;
+		}
+
+		++eoh;
+		++pos;
+	}
+
+	*eol = '\0';
 
 	return ret;
 }
